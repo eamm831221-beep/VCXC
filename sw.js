75972b723d369c9d6f0d801b55bb52eafe5aa4fb
eamm831221-beep <eamm831221@gmail.com>
2026-09-23 15:14:09 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,39 @@
+const CACHE_NAME = 'vcxc-cache-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://postimg.cc',
+  'https://postimg.cc',
+  'https://bienestar.gob.mx'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
 
